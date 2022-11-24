d4c16b8aa32f89f476100f33a013dd4af582a01f
Vyachaslav75 <kslava@tut.by>
2022-11-24 22:26:44 +0300

    Add program in folder test_1

---
+++ Test_1/Test.cs
@@ -0,0 +1,52 @@
+﻿int enterNumber(string msg, string errorMsg)
+{
+    int number;
+    while (true)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine(errorMsg);
+    }
+}
+
+string[] fillArray()
+{
+    int N = enterNumber("Введите количество элементов массива:  ", "Введите целое число");
+    string[] array = new string[N];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите строку:  ");
+        array[i] = Console.ReadLine() ?? "";
+    }
+    return array;
+}
+
+
+void PrintArray(string[] inArray)
+{
+    Console.Write("['{0}", string.Join("', '", inArray) + "'] -> ");
+    string res = String.Empty;
+    foreach (var i in inArray)
+    {
+        if (i.Length < 4)
+        {
+            if (res.Length > 1)
+            {
+                res = res + ", '" + i + "'";
+            }
+            else
+            {
+                res = "'" + i + "'";
+            }
+        }
+    }
+    Console.Write(res);
+}
+
+
+Console.Clear();
+string[] array = fillArray();
+PrintArray(array);
